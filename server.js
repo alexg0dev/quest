c288c5aac4a718da254932c5c20916fd58c5cd0e
alexg0dev <98514661+alexg0dev@users.noreply.github.com>
2025-01-17 21:30:48 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = '8080';
 
 // ======== CONFIGURATION ========
 
